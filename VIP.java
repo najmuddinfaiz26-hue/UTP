8874a381351ce34ec043f488d44ed88d8569bb14
Udeen <najmuddinfaiz26@gmail.com>
2026-04-13 22:38:08 +0700

    Refactor VIP constructor and method formatting

--- VIP.java
+++ VIP.java
@@ -2,13 +2,13 @@
 //Muhammad Faiz Najmuddin
 //255150707111013
 class VIP extends Member {
-    public VIP(String id, String nama) {
-        super(id, nama);
+public VIP(String id, String nama) {
+super(id, nama);
     }
 
-    @Override
-    public int hitungPembayaran(int hargaDasar, int sesi) {
-        int harga = hargaDasar * sesi;
+  @Override
+   public int hitungPembayaran(int hargaDasar, int sesi) {
+   int harga = hargaDasar * sesi;
 
         if (sesi > 5) {
             harga -= (harga * 10) / 100;
