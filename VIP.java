d54090b9785d43daa735a8236070765d8a5790d7
Udeen <najmuddinfaiz26@gmail.com>
2026-04-13 22:30:36 +0700

    Create VIP.java

---
+++ VIP.java
@@ -0,0 +1,22 @@
+ package UTP;
+//Muhammad Faiz Najmuddin
+//255150707111013
+class VIP extends Member {
+    public VIP(String id, String nama) {
+        super(id, nama);
+    }
+
+    @Override
+    public int hitungPembayaran(int hargaDasar, int sesi) {
+        int harga = hargaDasar * sesi;
+
+        if (sesi > 5) {
+            harga -= (harga * 10) / 100;
+        }
+
+        harga -= (harga * 15) / 100;
+
+        int pajak = (harga * 5) / 100;
+        return harga + pajak;
+    }
+}
